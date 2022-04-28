a8b5cbcff9acf1eb997e013387ac7b061806673f
CooperGidge <98977687+CooperGidge@users.noreply.github.com>
2022-04-28 13:57:48 -0400

    Better terminal guide, fixed bugs, input cells

--- src/cancer/Cancer.java
+++ src/cancer/Cancer.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package cancer;
+import java.util.Scanner;
 
 /*
 ========================================================
@@ -21,31 +22,61 @@ public class Cancer {
     public static String grid[][];
     public static int blobSize;
     
+    public static int rowSize;
+    public static int colSize;
+    
+    public static int row;
+    public static int col;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int row, col;
+        
+        Scanner scanInt = new Scanner(System.in);
+        
+        System.out.println("Enter number of rows:");
+        rowSize = scanInt.nextInt();
+        System.out.println("Enter number of columns:");
+        colSize = scanInt.nextInt();
         //Create 2D array size 12 x 12
-        grid = new String[12][12];
+        grid = new String[rowSize][colSize];
 
-        //Fill the array with blanks
-        for (row = 0; row < 12; row++) {
-          for (col = 0; col < 12; col++) {
+        //Fill the array with plus (+)
+        for (row = 0; row < rowSize; row++) {
+          for (col = 0; col < colSize; col++) {
             grid[row][col] = "+";
           }
         }
-
-        //Fill 70 random elements in the array with an asterisk
-        //Do no choose an element along the border
-        //The border will always contain elements with spaces
-        //(blank border)
-        for (int i = 0; i < 40; i++) {
-          row = (int) (Math.random() * 10 + 1);
-          col = (int) (Math.random() * 10 + 1);
-          grid[row][col] = "-";
+        
+        System.out.println("Do you want to input cells manually or generate random cells automatically?");
+        System.out.println("1 - Input cells manually");
+        System.out.println("2 - Generate random cells automatically");
+        
+        Scanner scanString = new Scanner(System.in);
+        
+        String ans = scanString.nextLine();
+        
+        if (ans.equals("1")) {
+            System.out.println("Enter either a plus (+) or minus (-)");
+            System.out.println("in order from top left to bottom right");
+            System.out.println("(excluding borders)");
+            inputCells();
+        } else if (ans.equals("2")) {
+            System.out.println("Randomizing cells...");
+            randomizeCells();
         }
+
+//        //Fill 70 random elements in the array with an asterisk
+//        //Do no choose an element along the border
+//        //The border will always contain elements with spaces
+//        //(blank border)
+//        for (int i = 0; i < 40; i++) {
+//          row = (int) (Math.random() * (rowSize - 2) + 1);
+//          col = (int) (Math.random() * (colSize - 2) + 1);
+//          grid[row][col] = "-";
+//        }
 
         //Print out the current grid
         displayGrid();
@@ -57,12 +88,12 @@ public class Cancer {
         //border and remove the blob at that location
         //NOTE: if a blank is chosen, the blob size is 0
         //and nothing is removed
-        int blobRow = (int) (Math.random() * 10 + 1);
-        int blobCol = (int) (Math.random() * 10 + 1);
+        int blobRow = (int) (Math.random() * (rowSize - 2) + 1);
+        int blobCol = (int) (Math.random() * (rowSize - 2) + 1);
         //blobRow = 2;
         //blobCol = 2;
 
-        System.out.println("The blob at " + blobRow + "," + blobCol
+        System.out.println("The blob at " + (blobRow+1) + "," + (blobCol+1)
           + " will be removed.");
         floodFill(blobRow, blobCol);
         System.out.println("The blob had " + blobSize +
@@ -70,6 +101,32 @@ public class Cancer {
         System.out.println("The new grid is:");
         //Print out the new grid
         displayGrid();
+    }
+    
+    public static void inputCells() {
+        Scanner scanString = new Scanner(System.in);
+        for (int row = 1; row < (rowSize-1); row++) {
+            System.out.println("\nEntering cells on row " + (row+1) + " (excluding borders)");
+          for (int col = 1; col < (colSize-1); col++) {
+            String cellType = scanString.nextLine();
+            if ( !(cellType.equals("+") || cellType.equals("-")) ) {
+                cellType = "+";
+            }
+            grid[row][col] = cellType;
+          }
+        }
+    }
+    
+    public static void randomizeCells() {
+        //Fill 70 random elements in the array with an asterisk
+        //Do no choose an element along the border
+        //The border will always contain elements with spaces
+        //(blank border)
+        for (int i = 0; i < 40; i++) {
+          row = (int) (Math.random() * (rowSize - 2) + 1);
+          col = (int) (Math.random() * (colSize - 2) + 1);
+          grid[row][col] = "-";
+        }
     }
     
     public static void floodFill(int row, int col) {
@@ -88,14 +145,15 @@ public class Cancer {
   }
 
   public static void displayGrid() {
-    String output = " 01234567890\n";
-    for (int row = 0; row <= 11; row++) {
-      if ((row > -1) && (row < 11)) {
-        output += (row % 10);
-      } else {
-        output += " ";
-      }
-      for (int col = 0; col <= 11; col++) {
+//    String output = " 01234567890\n";
+    String output = "\n";
+    for (int row = 0; row < rowSize; row++) {
+//      if ((row > -1) && (row < 11)) {
+//        output += (row % 10);
+//      } else {
+//        output += " ";
+//      }
+      for (int col = 0; col < colSize; col++) {
         output += grid[row][col];
       }
       output += "\n";
