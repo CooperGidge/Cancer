ff8870b65319713a6dfcad70a36e6bc6d90550c1
CooperGidge <98977687+CooperGidge@users.noreply.github.com>
2022-04-29 13:50:24 -0400

    Practically finished + comments

--- src/cancer/Cancer.java
+++ src/cancer/Cancer.java
@@ -1,21 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Cancer.java
  */
 package cancer;
-import java.util.Scanner;
 
-/*
-========================================================
-WIP
-- Based off BRIGHTSPACE lesson
-TODO
-- Fix intend spacing inconsistencies
-- Add better comments / javadoc
-- Finish
-========================================================
-*/
+// Import Scanner to make scanning for user input possible.
+import java.util.Scanner;
 
 /**
  *
@@ -23,140 +12,194 @@ TODO
  */
 public class Cancer {
 
+    // 2D array to represent the grid of cells
     public static String grid[][];
-    public static int blobSize;
     
+    // ints to hold the current row number and column number. Used in some calculations.
+    public static int row;
+    public static int col;
+    
+    // ints to hold the row size and column size.
+    // Useful for calculations that makes changing the rows/columns very easy,
+    // just in case they ever need to be changed.
     public static int rowSize;
     public static int colSize;
     
-    public static int row;
-    public static int col;
+    // int to count how many cancer blobs are found
+    public static int blobsFound = 0;
     
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
+        /*
+        // Create a Scanner to scan the rows and columns the user wants
         Scanner scanInt = new Scanner(System.in);
         
+        // Ask the user how many rows they want and hold their answer
+        // in the public static int rowSize.
         System.out.println("Enter number of rows:");
         rowSize = scanInt.nextInt();
+        
+        // Ask the user how many columns they want and hold their answer
+        // in the public static int colSize.
         System.out.println("Enter number of columns:");
         colSize = scanInt.nextInt();
-        //Create 2D array size 12 x 12
+        */
+        
+        // Create a 2D array (grid) with 15 rows and 15 columns.
+        // To allow the user to customize the amount of rows and columns,
+        // comment out the two lines below and uncomment the above code.
+        rowSize = 15;
+        colSize = 15;
         grid = new String[rowSize][colSize];
 
-        //Fill the array with plus (+)
+        // Before adding minus (-) signs, fill the array with plus (+) signs.
         for (row = 0; row < rowSize; row++) {
-          for (col = 0; col < colSize; col++) {
-            grid[row][col] = "+";
-          }
+            for (col = 0; col < colSize; col++) {
+                grid[row][col] = "+";
+            }
         }
         
+        // Present the user with a qustion:
+        // Input cells manually or
+        // Generate them randomly.
         System.out.println("Do you want to input cells manually or generate random cells automatically?");
         System.out.println("1 - Input cells manually");
         System.out.println("2 - Generate random cells automatically");
         
+        // Create a new Scanner to scan the user's answer to the question.
+        // Answer will be stored as a String to fix errors caused by inputting
+        // a String when Java is looking for an int.
         Scanner scanString = new Scanner(System.in);
         
+        // String to store the user's answer to the question.
         String ans = scanString.nextLine();
         
+        // If...
         if (ans.equals("1")) {
-            System.out.println("Enter either a plus (+) or minus (-)");
+            // The user says 1, print instructions and call the
+            // inputCells() method for the user to input their own cells.
+            System.out.println("\nEnter either a plus (+) or minus (-)");
             System.out.println("in order from top left to bottom right");
             System.out.println("(excluding borders)");
+            System.out.println("\nTip:\nPress \"enter\" to speed type a plus (+),");
+            System.out.println("as it will be registered as a plus (+).");
             inputCells();
-        } else if (ans.equals("2")) {
-            System.out.println("Randomizing cells...");
+        } else {
+            // The user says 2, or any other value, print the notice and call the
+            // randomizeCells() method to randomly add minus (-) cells
+            // (This is the default option - if the user doesn't input 1 or 2, this
+            // will be called, hence the lack of a condition.
+            System.out.println("\nRandomizing cells...");
             randomizeCells();
         }
 
-//        //Fill 70 random elements in the array with an asterisk
-//        //Do no choose an element along the border
-//        //The border will always contain elements with spaces
-//        //(blank border)
-//        for (int i = 0; i < 40; i++) {
-//          row = (int) (Math.random() * (rowSize - 2) + 1);
-//          col = (int) (Math.random() * (colSize - 2) + 1);
-//          grid[row][col] = "-";
-//        }
-
-        //Print out the current grid
+        // Print the current grid, now containing plus (-) and minus (-) signs
+        // (before the removal of minis (-) blobs)
         displayGrid();
-
-        //variable to determine the size of the blob
-        blobSize = 0;
-
-        //Pick one random element in the array that is not along the
-        //border and remove the blob at that location
-        //NOTE: if a blank is chosen, the blob size is 0
-        //and nothing is removed
-        int blobRow = (int) (Math.random() * (rowSize - 2) + 1);
-        int blobCol = (int) (Math.random() * (rowSize - 2) + 1);
-        //blobRow = 2;
-        //blobCol = 2;
-
-        System.out.println("The blob at " + (blobRow+1) + "," + (blobCol+1)
-          + " will be removed.");
-        floodFill(blobRow, blobCol);
-        System.out.println("The blob had " + blobSize +
-          " items in it");
-        System.out.println("The new grid is:");
-        //Print out the new grid
-        displayGrid();
-    }
-    
-    public static void inputCells() {
-        Scanner scanString = new Scanner(System.in);
+        
+        // Loop through each cell (excluding the borders)
         for (int row = 1; row < (rowSize-1); row++) {
-            System.out.println("\nEntering cells on row " + (row+1) + " (excluding borders)");
-          for (int col = 1; col < (colSize-1); col++) {
-            String cellType = scanString.nextLine();
-            if ( !(cellType.equals("+") || cellType.equals("-")) ) {
-                cellType = "+";
+            for (int col = 1; col < (colSize-1); col++) {
+                // If the cell is bad, run the floodFill to remove the blob.
+                // Else, do nothing and move on.
+                if ( grid[row][col].equals("-") ) {
+                    blobsFound++;
+                    floodFill(row, col);
+                }
             }
-            grid[row][col] = cellType;
-          }
+        }
+        
+        // Display how many blobs were found
+        System.out.println("The file had " + blobsFound + " cancer spots in it");
+        System.out.println("The new grid is");
+        
+        // Print the new grid
+        displayGrid();
+    }
+    
+    /**
+     * Allows the user to input their own cells
+     */
+    public static void inputCells() {
+        // Create a new Scanner to scan the user's input
+        Scanner scanString = new Scanner(System.in);
+        
+        // loop through each cell.
+        for (int row = 1; row < (rowSize-1); row++) {
+            // On each new row, notify the user what row they are on with some instructions.
+            System.out.println("\nEntering cells on row " + (row+1) + " (excluding borders)");
+            System.out.println("Enter cell then press \"enter\" to move onto the next cell.");
+            for (int col = 1; col < (colSize-1); col++) {
+                // Hold the user's input.
+                String cellType = scanString.nextLine();
+                // If the user's input is not a plus (+) or a minus (-)
+                // then default the cell to plus (+).
+                if ( !(cellType.equals("+") || cellType.equals("-")) ) {
+                    cellType = "+";
+                }
+                // Add the cell to the grid.
+                grid[row][col] = cellType;
+            }
         }
     }
     
+    /**
+     * Fills 40 random elements in the array (that are not on the border)
+     * with a minus (-)
+     */
     public static void randomizeCells() {
-        //Fill 70 random elements in the array with an asterisk
-        //Do no choose an element along the border
-        //The border will always contain elements with spaces
-        //(blank border)
+        // Replace 40 random cells (not on the border) with a minus (-) sign.
         for (int i = 0; i < 40; i++) {
-          row = (int) (Math.random() * (rowSize - 2) + 1);
-          col = (int) (Math.random() * (colSize - 2) + 1);
-          grid[row][col] = "-";
+            // Generate a random row (not on the border)
+            row = (int) (Math.random() * (rowSize - 2) + 1);
+            // Generate a random column (not on the border)
+            col = (int) (Math.random() * (colSize - 2) + 1);
+            // Replace the cell with a minus (-).
+            grid[row][col] = "-";
         }
     }
     
+    /**
+     * Checks the inputted cell
+     * If it contains a minus (-) sign, check all around it
+     * and replace each instance of a minus (-) with a space
+     */
     public static void floodFill(int row, int col) {
+        // If the cell is a minus (-)...
         if (grid[row][col].equals("-")) {
-          grid[row][col] = " ";
-          blobSize++;
-          floodFill(row - 1, col - 1);
-          floodFill(row - 1, col);
-          floodFill(row - 1, col + 1);
-          floodFill(row, col - 1);
-          floodFill(row, col + 1);
-          floodFill(row + 1, col - 1);
-          floodFill(row + 1, col);
-          floodFill(row + 1, col + 1);
+            // Replace the cell with a space
+            grid[row][col] = " ";
+            // Check each cell around it using this very method (recursion).
+            floodFill(row - 1, col - 1);
+            floodFill(row - 1, col);
+            floodFill(row - 1, col + 1);
+            floodFill(row, col - 1);
+            floodFill(row, col + 1);
+            floodFill(row + 1, col - 1);
+            floodFill(row + 1, col);
+            floodFill(row + 1, col + 1);
         }
-  }
-
-  public static void displayGrid() {
-    String output = "\n";
-    for (int row = 0; row < rowSize; row++) {
-      for (int col = 0; col < colSize; col++) {
-        output += grid[row][col];
-      }
-      output += "\n";
     }
-    System.out.println(output);
-  }
+    
+    /**
+     * Display the grid
+     */
+    public static void displayGrid() {
+        // create a String output to hold the grid.
+        String output = "\n";
+        // For each cell, add it to the String output
+        for (int row = 0; row < rowSize; row++) {
+            for (int col = 0; col < colSize; col++) {
+                output += grid[row][col];
+            }
+            // At the end of each row, add a line break.
+            output += "\n";
+        }
+        // Print the grid
+        System.out.println(output);
+    }
     
 }
